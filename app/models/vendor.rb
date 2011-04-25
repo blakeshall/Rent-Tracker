7029802b85949ef369c20c2c3bf91b7c4346887f
blakeshall <blake@blakeshall.com>
2011-04-25 11:25:33 -0400

    Fixed Unpaid/paid status

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,13 +1,29 @@
 class Vendor < ActiveRecord::Base
   has_many :payments , :dependent => :destroy
   
-  def current
-    v = Vendor.find(params[:id])
-    if v.weekly == true
-      "TRUE" 
+  def payments_between(start_date, end_date)
+   Payment.where("vendor_id = ? AND created_at BETWEEN ? AND ?", id, start_date, end_date)
+  end
+
+  def recent_payments
+   start_date = weekly? ? Date.today.ago(1.week) : Date.today.ago(1.month)
+
+   payments_between(start_date, Time.now)
+  end
+
+  def total_paid
+    if recent_payments.map(&:amount).reduce(&:+) == nil
+      0
     else
-      "FALSE"
+      recent_payments.map(&:amount).reduce(&:+)
     end
-    
+  end
+
+  def paid_up?
+   debt <= 0
+  end
+
+  def debt
+   amount - total_paid
   end
 end
